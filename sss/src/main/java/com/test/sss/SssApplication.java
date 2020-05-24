4ff3252d034f4e4dfe70239db6b0ca96dd533be3
shenzp <szp12138@163.com>
2020-05-24 18:22:13 +0800

    什么也没做

---
+++ sss/src/main/java/com/test/sss/SssApplication.java
@@ -0,0 +1,13 @@
+package com.test.sss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SssApplication.class, args);
+    }
+
+}
---
+++ sss/src/test/java/com/test/sss/SssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
